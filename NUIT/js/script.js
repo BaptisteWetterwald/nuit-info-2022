aeeb008cd7e287d0e9018374fd4858660c4ce9e1
TinySharky <104091661+TinySharky@users.noreply.github.com>
2022-12-02 01:12:52 +0100

    Add files via upload

---
+++ NUIT/js/script.js
@@ -0,0 +1,35 @@
+var btn = document.getElementById("play");
+var perso = document.getElementById("perso");
+var retour = document.getElementById("return");
+var fond = document.getElementById("image-fond");
+var all = document.getElementById("header");
+var perso1 = document.getElementById("perso1");
+var perso2 = document.getElementById("perso2");
+
+btn.addEventListener('click', ()=>{
+    btn.style.transform = 'translateY(200px)';
+    setTimeout(visible, 250);
+});
+
+perso1.addEventListener('click', ()=>{
+    btn.style.transition = 'none';
+    all.style.visibility = 'hidden';
+    perso.style.visibility = 'hidden';
+    fond.style.filter = 'none';
+});
+
+perso2.addEventListener('click', ()=>{
+    btn.style.transition = 'none';
+    all.style.visibility = 'hidden';
+    perso.style.visibility = 'hidden';
+    fond.style.filter = 'none';
+});
+
+retour.addEventListener('click', ()=>{
+    perso.style.visibility = 'hidden';
+    btn.style.transform = 'translateY(0px)';
+})
+
+function visible(){
+    perso.style.visibility = 'visible'
+}
